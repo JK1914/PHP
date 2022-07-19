e6d6039dc9e248066d391d53fe10a251dfee3543
Oleg <collector1913@gmail.com>
2022-07-19 18:09:50 +0600

    hw_02

--- index.php
+++ index.php
@@ -1,44 +1,123 @@
+<h3>Калькулятор</h3>
+<form action="" method="GET">
+    Введите число 1: <input type="text" name="num1"><br>
+    Введите число 2: <input type="text" name="num2"><br>
+    <p>Введите номер операции: <input type="text" name="op"></p>
+    <p><input type="submit" value="Получить результат"></p>
+</form>
+
+
 <?php
 
-class Math{    
-    	
-    
-        function SumFunc($x, $y){
-        	return $x+$y;
+class MathFactory
+{
+    public function getMathFunc ($type)
+    {   
+        switch($type)
+        {
+            case 1:
+                $funcName = new SumFunc();
+                break;
+            case 2:
+                $funcName = new DifFunc();
+                break;
+            case 3:
+                $funcName = new MultFunc();
+                break;
+            case 4:
+                $funcName = new DivFunc();
+                break;
+            case 5:
+                $funcName = new PowFunc();
+                break;
+            case 6:
+                $funcName = new SqrtFunc();
+                break;
+            default:
+                echo 'Неправильный тип!';                
         }
-        
-        function DifFunc($x, $y){
-        	return $x-$y;
-        }
-        
-        function MultFunc($x, $y){
-        	return $x*$y;
-        }
-        
-        function DivFunc($x, $y){
-        	if($y!=0)
-            {
-            	return $x/$y;
-            }
-        	else
-            {
-            	print('Деление на ноль!');
-            }
-        }
-        
-        function PowFunc($x, $y){        	
-        	return pow($x, $y);
-        }
-        
-        function SqrtFunc($x, $y){        	
-        	return sqrt($x, $y);
-        }
+        return $funcName;
     }
-    
-    
-$math = new Math;
+}
 
-print 'Результат: ';
-print $math->DivFunc(10,5);
-    
+interface Func 
+{
+    public function Count($x,$y);
+}
+
+class SumFunc implements Func 
+{
+    public function Count($x,$y)
+    {        
+        echo 'Сложение<br>';
+        echo 'Результат: ' . $x+$y;
+    }
+}
+
+class DifFunc implements Func 
+{
+    public function Count($x,$y)
+    {
+        echo 'Вычитание<br>';
+        echo 'Результат: ' . $x-$y;
+    }
+}
+
+class MultFunc implements Func 
+{
+    public function Count($x,$y)
+    {
+        echo 'Умножение<br>';
+        echo 'Результат: ' . $x*$y;
+    }
+}
+
+class DivFunc implements Func 
+{
+    public function Count($x,$y)
+    {
+        if ($y!=0)
+        {
+            echo 'Деление<br>';
+            echo 'Результат: ' . $x/$y;
+        }
+        else
+        {
+            echo 'Деление на ноль!';
+        }   
+    }
+}
+
+class PowFunc implements Func 
+{
+    public function Count($x,$y)
+    {
+        echo 'Возведение в степень<br>';
+        echo 'Результат: ' . pow($x, $y);
+    }
+}
+
+class SqrtFunc implements Func 
+{
+    public function Count($x,$y)
+    {
+        echo 'Извлечение корня<br>';
+        echo 'Результат: ' . sqrt($x);
+    }
+}
+
+
+
+if (isset($_GET['num1']) and isset($_GET['num2']) and isset($_GET['op']))
+{
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
+    $op = $_GET['op'];
+
+    $math = new MathFactory();
+    $res = $math->getMathFunc($op);
+    $res->Count($num1,$num2);
+}
+
+
 ?>
