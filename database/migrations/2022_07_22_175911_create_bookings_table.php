2114976e972ba063f1e390d293add40488fa61ec
Oleg <collector1913@gmail.com>
2022-08-16 17:50:00 +0600

    booking

---
+++ database/migrations/2022_07_22_175911_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('room_id');
+            $table->boolean('booked');
+            $table->integer('user_id');            
+            $table->timestamp('time_start');
+            $table->timestamp('time_end');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
