c61d835ed4e92c38607edcf2b2df5ed568bb7121
Oleg <collector1913@gmail.com>
2022-08-18 18:00:23 +0600

    add: CustomException

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\CustomException;
 use Illuminate\Http\Request;
 use App\Models\Room;
 
@@ -9,7 +10,7 @@ class RoomsController extends Controller
 {
     public function create(Request $request)
     {
-        $rooms = new Rooms();
+        $rooms = new Room();
         $rooms->number = $request->number;
         $rooms->square = $request->square;
         $rooms->has_pojector = $request->has_pojector;
@@ -19,30 +20,37 @@ class RoomsController extends Controller
     
     public function list()
     {
-        $rooms = Rooms::get();
+        $rooms = Room::get();
         return $rooms;        
     } 
 
     public function item($id)
     {
-        $rooms = Rooms::where('id', $id)->first();
+        $rooms = Room::where('id', $id)->first();
         return $rooms;        
     } 
 
     public function update($id, Request $request)
     {
-        $rooms = Rooms::where('id', $id)->first();
-        $rooms = new Rooms();
-        $rooms->number = $request->number;
-        $rooms->square = $request->square;
-        $rooms->has_pojector = $request->has_pojector;
-        $rooms->building_id = $request->building_id;        
-        $rooms->save();
-        return $rooms;      
+        if (empty($id))
+        {
+            throw new CustomException("Id пустой!");            
+        }
+        else
+        {
+            $rooms = Room::where('id', $id)->first();
+            $rooms = new Room();
+            $rooms->number = $request->number;
+            $rooms->square = $request->square;
+            $rooms->has_pojector = $request->has_pojector;
+            $rooms->building_id = $request->building_id;        
+            $rooms->save();
+            return $rooms; 
+        }           
     } 
 
     public function delete($id)
     {
-        $rooms = Rooms::where('id', $id)->delete();        
+        $rooms = Room::where('id', $id)->delete();        
     } 
 }
